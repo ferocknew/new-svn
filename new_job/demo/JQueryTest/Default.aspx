6e692d6ea784844c6b21a46eeca1f7dade2d0957
Todayn.zheng <Todayn.zheng@aa7b40e8-dd02-11de-b878-c5ec750a8c44>
2010-01-14 06:16:43 +0000

    新增调用webservice: GetInfos : 返回Info咨询

--- new_job/demo/JQueryTest/Default.aspx
+++ new_job/demo/JQueryTest/Default.aspx
@@ -5,7 +5,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head runat="server">
     <title>Untitled Page</title>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<%--    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>--%>
+    <script type="text/javascript" src="JS/jquery-1.4a2.js"></script>
     <script type="text/javascript" src="files/action.js"></script>
 </head>
 <body>
---
+++ new_job/demo/WebService/App_Code/GetInfos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using TestFunctions;
+using System.Data;
+using System.Data.SqlClient;
+
+/// <summary>
+/// Summary description for GetInfos
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class GetInfos : System.Web.Services.WebService {
+
+    public GetInfos () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld() {
+        return "Hello World";        
+        
+    }
+
+    [WebMethod]
+    public List<InfoBody> GetInfoList(string siteName,string comName)
+    {
+        InfoList Infos = new InfoList();
+
+        return Infos.GetInfo(siteName, comName);
+        //return siteName;
+    }
+    
+}
+
