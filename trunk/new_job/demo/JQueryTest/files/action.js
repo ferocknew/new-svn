d5a5b68f7ab07e2387fcbe6facadcf6ec1beb570
Todayn.zheng <Todayn.zheng@aa7b40e8-dd02-11de-b878-c5ec750a8c44>
2010-01-14 06:16:43 +0000

    新增调用webservice: GetInfos : 返回Info咨询

--- trunk/new_job/demo/JQueryTest/files/action.js
+++ trunk/new_job/demo/JQueryTest/files/action.js
@@ -1,15 +1,21 @@
-$(function(){
-	$("#TestButton").click(function(){
-		$.ajax({
-			type: "POST",
-			contentType: "application/json",
-			url: "../WebService/TestWS01.asmx/HelloWorld",
-			data: "{}",
-			dataType: "json",
-			success: function(json){
-				var html_="Name:" + json.d.Name + "  Pwd:" + json.d.Pwd;
-				 $("#divTest").html(html_); 
-			}
-		});
-	});
+$(function() {
+    $("#TestButton").click(function() {
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: "../WebService/GetInfos.asmx/GetInfoList",
+            data: "{siteName:'',comName:'mrThink'}",
+            dataType: "json",
+            success: function(result) {
+                $(result.d).each(function() {
+                    $("#divTest").append("SiteName:"+this["SiteName"]+"  "+"ComName:"+this["ComName"]);
+                });
+            },
+            error: function(json) {
+                var html_ = "error";
+                $("#divTest").html(html_);
+            }
+
+        });
+    });
 });
